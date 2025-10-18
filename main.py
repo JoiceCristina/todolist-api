55bd198fbe60af4174479d235eb03b0d9891661d
Joice <joice.cristina.2403@gmail.com>
2025-10-17 22:07:26 -0300

    Tarefas

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask;
+from tarefas import buscar_tarefas
 
 app = Flask(__name__)
 
 @app.route('/api')
 def index():
     return 'Api rodando'
+
+@app.route('/api/tarefas')
+def get_tarefas():
+    tarefas = buscar_tarefas()
+    return tarefas
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tarefas.py
@@ -0,0 +1,15 @@
+def buscar_tarefas():
+    tarefas = [
+        {
+            "id": 1,
+            "tarefa": "Aprender Python",
+            "status": "Pendente",
+        },
+        {
+            "id": 2,
+            "tarefa": "Aprender Inglês",
+            "status": "Pendente",
+        },
+
+    ]
+    return tarefas
